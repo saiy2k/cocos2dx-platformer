d626ef86aa0c55f9559c49cfd0d2562c453f0f65
saiy2k <saiy2k@gmail.com>
2013-01-22 13:10:57 +0530

    Initial Commit

---
+++ LineInterpolation/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  LineInterpolationAppController.h
+//  LineInterpolation
+//
+//  Created by saiy2k on 22/01/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ LineInterpolation/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LineInterpolation
+//
+//  Created by saiy2k on 22/01/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
